03cd51c6eb400e3225258d17dec1fe9db890e6b9
Leon Dorsch <leon.dorsch@gmx.de>
2025-06-29 15:43:21 +0200

    first component created

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-
-  </div>
+  <BaseCard>
+    <h2>Hallo Welt</h2>
+    <p>Dies ist ein Inhalt in der Karte.</p>
+  </BaseCard>
 
 </template>
 
 <script>
-
+import BaseCard from './components/BaseCard.vue';
 
 export default {
   name: 'App',
   components: {
+    BaseCard
   }
 }
 </script>
 
 <style>
-
 body {
   margin: 0;
 }
+
 #app {
   background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
+  box-sizing: border-box;
+  padding: 100px;
   width: 100vw;
-  height: 100vh;
+  min-height: 100vh;
 }
 </style>
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="base-card">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.base-card {
+  background: #011F35;
+  border-radius: 16px;
+  padding: 20px 32px;
+  color: white; 
+}
+</style>
