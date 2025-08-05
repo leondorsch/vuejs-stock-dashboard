332c6b8cefddf1cf555f767342967c14c31d7087
Leon Dorsch <leon.dorsch@gmx.de>
2025-08-05 15:48:31 +0200

    added service

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
-  <BaseCard/>
+  <BaseCard />
 </template>
 
 <script>
 
 import BaseCard from './components/BaseCard.vue'
-
+import { stockService } from './services/stockService'
 
 export default {
   name: 'App',
   components: {
     BaseCard
+  },
+  async created() {
+    this.data = await stockService.getRevenue('$AAPL');
+    console.log(this.data);
   }
 }
 </script>
@@ -19,6 +23,7 @@ export default {
 body {
   margin: 0;
 }
+
 #app {
   background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
   min-height: 100vh;
@@ -26,5 +31,4 @@ body {
   padding: 100px;
   box-sizing: border-box;
 }
-
 </style>
