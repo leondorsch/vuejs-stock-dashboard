772c1633c9dad9da07cdbdf75ad116e3d4d9d7c7
Leon Dorsch <leon.dorsch@gmx.de>
2025-08-07 19:16:23 +0200

    added header and components

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
-  <BaseCard />
+  <HeadComponent/>
+  <RevenueCard />
 </template>
 
 <script>
 
-import BaseCard from './components/BaseCard.vue'
+import HeadComponent from './components/HeadComponent.vue';
+import RevenueCard from './components/RevenueCard.vue';
 import { stockService } from './services/stockService'
 
 export default {
   name: 'App',
   components: {
-    BaseCard
+    HeadComponent,
+    RevenueCard,
   },
   async created() {
     this.data = await stockService.getRevenue('$AAPL');
@@ -22,13 +25,40 @@ export default {
 <style>
 body {
   margin: 0;
+  font-family: 'Rubik', sans-serif;
 }
 
 #app {
   background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
   min-height: 100vh;
   width: 100vw;
-  padding: 100px;
   box-sizing: border-box;
 }
+
+@font-face {
+  font-family: 'Rubik';
+  src: url('./assets/fonts/Rubik-Regular.ttf') format('truetype');
+  font-weight: 400;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'Rubik';
+  src: url('./assets/fonts/Rubik-Medium.ttf') format('truetype');
+  font-weight: 500;
+  font-style: medium;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'Rubik';
+  src: url('./assets/fonts/Rubik-SemiBold.ttf') format('truetype');
+  font-weight: 600;
+  font-style: normal;
+  font-display: swap;
+}
+
+
+
 </style>
---
+++ src/components/HeadComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="head-container">
+        <div class="blue-rectangle"></div>
+        <span>The Magnificent Seven Companies</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HeadComponent'
+}
+</script>
+
+<style>
+
+.head-container {
+    display: flex;
+    align-items: center;
+    position: relative;
+    padding-top: 64px;
+    gap: 24px;
+}
+
+.blue-rectangle {
+    background-color: #39DAFF;
+    width: 96px;
+    height: 32px;
+    border-top-right-radius: 32px;
+    border-bottom-right-radius: 32px;
+}
+
+.head-container {
+    display: flex;
+    color:#F9F9F9;
+    font-size: 36px;
+    font-weight: 500
+}
+</style>
